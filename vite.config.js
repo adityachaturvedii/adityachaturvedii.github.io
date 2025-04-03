8fe2c6b31990a8875ea953e4c3a6227c8db57951
Aditya Chaturvedi <chaturvediaditya910@gmail.com>
2025-04-03 07:38:02 +0530

    corrected

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/adityachaturvedii.github.io/',  // Add this line to specify the base path
+  base: '/',  // Add this line to specify the base path
 })
