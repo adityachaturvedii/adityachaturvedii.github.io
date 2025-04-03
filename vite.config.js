8bcca4fe7e65d877ae9b747dbbe7e63da1c1b945
Aditya Chaturvedi <chaturvediaditya910@gmail.com>
2025-04-03 07:31:35 +0530

    gh pages added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aditya_website/',  // Add this line to specify the base path
+  base: '/adityachaturvedii.github.io/',  // Add this line to specify the base path
 })
